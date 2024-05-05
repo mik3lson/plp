63335dd7248e8a837d1355e905dac670d5e1637b
unknown <ndudimichael06@gmail.com>
2024-05-05 22:52:12 +0100

    python commit

---
+++ plpassignment.py
@@ -0,0 +1,11 @@
+# prompt user for name
+name = input ("name:")
+
+#prompt user for age 
+age = str (input("age:"))
+
+#prompt user for location
+location = input("location:")
+
+#print a personalised message 
+print ("hello, your name is "+ name + " .You are " + age+ " years old and you stay at " + location )
